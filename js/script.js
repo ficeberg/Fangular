066bacbe3295f6cf3354e7ae7889221ae52591aa
Festum Qin <Festum@G.PL>
2014-08-28 01:07:37 +0800

    fix name issue for git

--- js/script.js
+++ js/script.js
@@ -42,12 +42,12 @@ var view = {
 		//get fb profile using perm token
 		var req = this.req;
 		var template = this.template;
-		console.log('run');
+		calert('run');
 		$.when($.ajax(req[0]),$.ajax(req[1]),$.ajax(req[2])).done(function(a1, a2, a3){
 			a1[0]['interests'] = a3[0]['data'];
 			a1[0]['picture'] = req[3];
 
-			console.log(a2[0]);
+			alert(a2[0]);
 
 			var model = Backbone.Model.extend({
 				defaults: {p: a1[0], r: marked(a2[0])}
@@ -128,7 +128,7 @@ app.on('initialize:after', function () {
 			}
 		});
 	});
-	console.log('here')
+	alert('here');
 	view.run();
 
 });
