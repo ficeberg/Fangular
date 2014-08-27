b57424bc0fc5b04c469ba0f140c310f95d624237
Festum Qin <Festum@G.PL>
2014-08-28 01:10:35 +0800

    fix name issue for git

--- js/script.js
+++ js/script.js
@@ -43,16 +43,16 @@ var view = {
 		var req = this.req;
 		var template = this.template;
 
-		$.when($.ajax(req[0]),$.ajax(req[1]),$.ajax(req[2])).done(function(a1, a2, a3){
+		$.when($.ajax(req[0]),$.ajax(req[2])).done(function(a1, a3){
 			a1[0]['interests'] = a3[0]['data'];
 			a1[0]['picture'] = req[3];
 
-			alert(a2[0]);
+			alert("test");
 
 			var model = Backbone.Model.extend({
-				defaults: {p: a1[0], r: marked(a2[0])}
+				defaults: {p: a1[0], r: ""}
 			});
-
+			alert("test2");
 			var view = {
 				p: new template.p({model:new model(), el : '#profile'}),
 				r: new template.r({model:new model(), el : '#readme'})
